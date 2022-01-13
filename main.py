d9836a1878f5941dd34149603fdad64b3f5e364e
EJShim <ejshim@imagoworks.ai>
2022-01-13 19:02:53 +0900

    oop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import vtk
 from vtk.util import numpy_support
 import numpy as np
+import math
+import utils
 
 
 #기본 셋업
@@ -15,36 +17,9 @@ ren.SetBackground(0.1, 0.2, 0.4)
 renWin.SetSize(1000, 1000)
 
 
-pointBuffer = np.array([
-        [1, 0, 0],
-        [2, 0, 0],
-        [3, 0, 0],
-        [4, 0, 0],
-        [5, 0, 0],        
-    ], dtype=np.float64)
-polydata = vtk.vtkPolyData()
-cubeGlyph = []
-
-def MakeSphereActor(polydata):
-    mapper = vtk.vtkOpenGLSphereMapper()
-    mapper.SetRadius(0.1)
-    mapper.SetInputData(polydata)
-    actor = vtk.vtkActor()
-    actor.SetMapper(mapper)
-
-    return actor
-
-def MakeActor(polydata):
-    mapper = vtk.vtkPolyDataMapper()
-    mapper.SetInputData(polydata)
-
-    actor = vtk.vtkActor()
-    actor.SetMapper(mapper)
-
-    return actor
-
-
 pickedId = -1
+
+targetGraph = None
 
 def MouseClickCallback(obj, e):    
     global pickedId
@@ -53,9 +28,9 @@ def MouseClickCallback(obj, e):
     picker = obj.GetPicker()
     eventPosition = obj.GetEventPosition()
     picker.Pick(float(eventPosition[0]),float(eventPosition[1]),0.0,ren)    
-    pickedId = picker.GetPointId()
+    pickedId = picker.GetPointId()    
 
-
+    
 
 def MouseMoveCallback(obj, r):
     
@@ -66,39 +41,29 @@ def MouseMoveCallback(obj, r):
 
     #Update Point
     pos = picker.GetPickPosition()
-    pointBuffer[pickedId][0] = pos[0]
-    pointBuffer[pickedId][1] = pos[1]
-    pointBuffer[pickedId][2] = pos[2]
-    polydata.GetPoints().Modified()
 
-    #Update Glyph
-    cubeGlyph[pickedId].SetPosition(pos)
+    targetGraph.updatePoint(pickedId, pos)
 
     renWin.Render()
 
-def MakeBoxGlyph():
-    source = vtk.vtkCubeSource()
-    source.SetCenter(0, 0, 0)
-    source.SetXLength(.6)
-    source.SetYLength(.6)
-    source.SetZLength(.6)
-    source.Update()
-
-    actor = MakeActor(source.GetOutput())
-    actor.GetProperty().SetColor(1, 0, 0)
-    actor.GetProperty().SetOpacity(.2)
-
-    return actor
-    
 
     
 
 def MouseReleaseCallback(obj, e):    
     global pickedId    
     pickedId = -1    
+    transBuffer = np.zeros_like(pointBuffer)
 
 if __name__ == "__main__":    
 
+    pointBuffer = np.array([
+        [1, 0, 0],
+        [2, 0, 0],
+        [3, 0, 0],
+        [4, 0, 0],
+        [5, 0, 0],        
+    ], dtype=np.float64)
+    polydata = vtk.vtkPolyData()
     points = vtk.vtkPoints()
     pointArray = numpy_support.numpy_to_vtk(pointBuffer)
     points.SetData(pointArray)
@@ -106,25 +71,15 @@ if __name__ == "__main__":
     line = vtk.vtkPolyLine()
     line.GetPointIds().SetNumberOfIds(pointBuffer.shape[0])
     for pid in range(pointBuffer.shape[0]):
-        line.GetPointIds().SetId(pid, pid)
-        cube = MakeBoxGlyph()
-        cube.SetPosition(pointBuffer[pid])
-        cubeGlyph.append(cube)
+        line.GetPointIds().SetId(pid, pid)        
     polys = vtk.vtkCellArray()
-    polys.InsertNextCell(line)
-    #vtkPolyData -> 3차원 오브젝트 정보를 가지고있는 객체    
+    polys.InsertNextCell(line)    
     polydata.SetPoints(points)
     polydata.SetLines(polys)
 
+    targetGraph = utils.DeformableGraph(polydata)
+    targetGraph.addToRenderer(ren)
 
-    pointActor = MakeSphereActor(polydata)
-    lineActor = MakeActor(polydata)
-
-    #렌더러에 오브젝트 추가
-    ren.AddActor(pointActor)
-    ren.AddActor(lineActor)
-    for cube in cubeGlyph:
-        ren.AddActor(cube)
 
 
     #refresh
---
+++ utils.py
@@ -0,0 +1,59 @@
+import vtk
+
+
+def MakeSphereActor(polydata):
+    mapper = vtk.vtkOpenGLSphereMapper()
+    mapper.SetRadius(0.1)
+    mapper.SetInputData(polydata)
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+
+    return actor
+
+def MakeActor(polydata):
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputData(polydata)
+
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+
+    return actor
+
+def MakeBoxGlyph():
+    source = vtk.vtkCubeSource()
+    source.SetCenter(0, 0, 0)
+    source.SetXLength(.6)
+    source.SetYLength(.6)
+    source.SetZLength(.6)
+    source.Update()
+
+    actor = MakeActor(source.GetOutput())
+    actor.GetProperty().SetColor(1, 0, 0)
+    actor.GetProperty().SetOpacity(.2)
+
+    return actor
+    
+
+
+class DeformableGraph():
+    def __init__(self, polydata):        
+        
+        self.polydata = polydata
+        
+        # self.polydata.ShallowCopy(polydata)
+        self.pointActor = MakeSphereActor(self.polydata)
+        self.actor = MakeActor(self.polydata)
+
+    def addToRenderer(self, renderer):
+        renderer.AddActor(self.pointActor)
+        renderer.AddActor(self.actor)
+
+
+    def updatePoint(self, idx, pos):
+        print(idx, pos)
+
+        self.polydata.GetPoints().SetPoint(idx, pos[0], pos[1], pos[2])
+        self.polydata.GetPoints().Modified()
+
+    def modified(self):
+        print("Solve Dynamics!")
